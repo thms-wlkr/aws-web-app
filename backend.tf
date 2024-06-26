1b4cd1a82534be1a9ae02bfe1fb5a8984af6f5b7
Thomas Walker <twalker@caci.co.uk>
2024-06-26 16:14:58 +0100

    add tf state file bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,17 @@
 terraform {
   backend "s3" {
-    bucket = "your-state-file-bucket"
-    key = "project-infrastrcture"
-    region = "us-east-1"
+    bucket = "demo-bucket"  // Replace with your bucket name
+    key    = "terraform.tfstate"
+    region = "eu-west-1"  // Replace with your preferred AWS region
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "demo-bucket"
+  acl    = "private" # Note: This will still work, but it's deprecated
+
+  tags = {
+    name      = "tf-state-bucket"
+    terraform = true
   }
 }
