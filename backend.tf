b4a3345d8a3d5174ddea53a4bacbc559eacff0d0
Thomas Walker <thomas1walker@outlook.com>
2024-07-12 15:47:44 +0100

    modify backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,59 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
-    bucket = "your-state-file-bucket"
-    key = "project-infrastrcture"
-    region = "us-east-1"
+    bucket         = "tf-state-bucket"
+    key            = "terraform/state"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "tf-state-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    name        = "terraform state bucket"
+    terraform   = "true"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    name        = "terraform lock table"
+    terraform   = "true"
   }
 }
